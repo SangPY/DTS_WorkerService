022fbe91a36bfb0e9ef06c3816f69e8ba78de1b1
sangdt <sangdt@vikings.vn>
2026-02-24 11:36:59 +0700

    Run successful worker service

--- DTS_WorkerService/Program.cs
+++ DTS_WorkerService/Program.cs
@@ -1,6 +1,14 @@
 using DTS_WorkerService;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "DTS Worker Service";
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
