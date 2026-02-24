339ab2cdb404f62d949a7e3d72201d67ce60d88e
sangdt <sangdt@vikings.vn>
2026-02-24 09:14:24 +0700

    Add cmd run as administrator

--- DTS_WorkerService/Worker.cs
+++ DTS_WorkerService/Worker.cs
@@ -17,7 +17,7 @@ namespace DTS_WorkerService
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
